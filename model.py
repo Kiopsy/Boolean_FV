7bf03240593023e320e8aec7984a73781e4110b1
Goncalves <victorgoncalves@college.harvard.edu>
2023-04-17 19:16:50 -0400

    Commiting Emeka's code

---
+++ model.py
@@ -0,0 +1,85 @@
+import random
+
+# Circuits were composed of up to
+
+# https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1000206#s4 
+# https://www.pnas.org/doi/full/10.1073/pnas.0503610102
+# https://www.pnas.org/doi/abs/10.1073/pnas.0611630104
+
+# Define the allowed operations
+operations = {"XOR": lambda x, y: not (x or y), 
+			  "EQ": lambda x, y: x == y, 
+			  "AND": lambda x, y: x and y,
+			  "OR": lambda x, y: x or y}
+
+# Define the maximum number of gates in the circuit
+max_gates = 12
+
+# Define the input and output variables
+inputs = ["x", "y", "w", "z"]
+output = "u"
+
+# Define the possible values for the inputs and outputs
+values = [0, 1]
+
+# Define the function to generate a random circuit
+def generate_circuit():
+    circuit = []
+    for i in range(random.randint(1, max_gates)):
+        gate = (random.choice(list(operations.keys())), random.randint(0, 3), random.randint(0, 3))
+        circuit.append(gate)
+    return circuit
+
+# Define the function to evaluate a circuit
+def evaluate_circuit(circuit, goal):
+    num_correct = 0
+    for x in values:
+        for y in values:
+            for w in values:
+                for z in values:
+                    inputs_dict = {"x": x, "y": y, "w": w, "z": z}
+                    output_dict = {output: None}
+                    for gate in circuit:
+                        op, in1, in2 = gate
+                        val1 = inputs_dict[inputs[in1]]
+                        val2 = inputs_dict[inputs[in2]]
+                        if op == "XOR":
+                            output_dict[gate] = int(not (val1 or val2))
+                        elif op == "EQ":
+                            output_dict[gate] = int(val1 == val2)
+                        elif op == "AND":
+                            output_dict[gate] = int(val1 and val2)
+                        elif op == "OR":
+                            output_dict[gate] = int(val1 or val2)
+                        inputs_dict[gate] = output_dict[gate]
+                    if output_dict[output] == goal(x, y, w, z):
+                        num_correct += 1
+    return float(num_correct) / (len(values) ** 4)
+
+# Define the goals as specified
+def g(x, y):
+    return random.choice([operations["XOR"], operations["EQ"]])(x, y)
+
+def h(w, z):
+    return random.choice([operations["XOR"], operations["EQ"]])(w, z)
+
+def f(a, b):
+    return random.choice([operations["AND"], operations["OR"]])(a, b)
+
+# Define the function to generate a new goal every E generations
+def get_goal(t):
+    if t % 20 == 0:
+        return lambda x, y, w, z: f(g(x, y), h(w, z))
+    else:
+        return current_goal
+
+# Initialize the current goal and circuit
+current_goal = lambda x, y, w, z: f(g(x, y), h(w, z))
+current_circuit = generate_circuit()
+
+# Evaluate the fitness of the circuit
+fitness = evaluate_circuit(current_circuit, current_goal)
+
+# Print the results
+print("Current circuit: ", current_circuit)
+print("Current fitness: ", fitness)
