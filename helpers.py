73d4e510c1736b75b2630be6f0baf79e68dec6a6
Emeka Ezike <franklinezike@gmail.com>
2023-04-24 14:10:48 -0400

    constant N

--- helpers.py
+++ helpers.py
@@ -15,7 +15,7 @@ def add_to_csv(file_path, data):
         writer = csv.writer(file)
         writer.writerow(data)
 
-def create_graph(gates, output, n = 4):
+def create_graph(gates, output, N = 4):
     
     TARGET = "*"
     GATE = "NAND"
@@ -42,7 +42,7 @@ def create_graph(gates, output, n = 4):
     G = nx.DiGraph()
 
     # Add nodes
-    inputs = [chr(i) for i in range(ord('z') - n + 1, ord('z') + 1)] # TODO
+    inputs = [chr(i) for i in range(ord('z') - N + 1, ord('z') + 1)] # TODO
     nodes = inputs + [f"{GATE}_{i+len(inputs)}" for i in range(len(gates))]
     nodes[output] = TARGET # output is target node
     G.add_nodes_from(nodes)
